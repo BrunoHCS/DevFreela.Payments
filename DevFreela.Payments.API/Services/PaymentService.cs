f411e2da42f99ffab0a6e62d3bc43d44f258ed12
Bruno Henrique <bruno_hcs2009@hotmail.com>
2023-10-03 15:34:48 -0300

    Adicionar arquivos de projeto.

---
+++ DevFreela.Payments.API/Services/PaymentService.cs
@@ -0,0 +1,14 @@
+﻿using DevFreela.Payments.API.Models;
+using System.Threading.Tasks;
+
+namespace DevFreela.Payments.API.Services
+{
+    public class PaymentService : IPaymentService
+    {
+        public Task<bool> Process(PaymentInfoInputModel paymentInfoInputModel)
+        {
+            //Aqui teria um Gateaway de pagamentos
+            return Task.FromResult(true);
+        }
+    }
+}
